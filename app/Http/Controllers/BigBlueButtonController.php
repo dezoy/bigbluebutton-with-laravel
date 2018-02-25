75a1c7f5691137aa988f582cf337c06e673b74c8
Anton <litto82@gmail.com>
2018-02-25 11:16:30 +0300

    Fixed code

--- app/Http/Controllers/BigBlueButtonController.php
+++ app/Http/Controllers/BigBlueButtonController.php
@@ -55,7 +55,7 @@ class BigBlueButtonController extends Controller
 
 	public function createMeeting()
 	{
-		if ( ! $request->isMethod('post') ){
+		if ( ! request()->isMethod('post') ){
 			return;
 		}
 
