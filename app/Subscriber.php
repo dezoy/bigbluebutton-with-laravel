091f9845498773d50185ca7c04edcdf20e15d3d1
Anton <litto82@gmail.com>
2018-02-25 18:34:36 +0300

    Cange class name

--- app/Subscriber.php
+++ app/Subscriber.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Meeting extends Model
+class Subscriber extends Model
 {
 	use SoftDeletes;
 
