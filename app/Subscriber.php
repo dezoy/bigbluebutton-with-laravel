6c06b0f57982d4c4dadc392ec3ac5023d9028789
Anton <litto82@gmail.com>
2018-02-25 18:27:34 +0300

    Revert "Change class name"
    This reverts commit fa2c0148194d2c38d49c8a07d1c62474141a4090.

--- app/Subscriber.php
+++ app/Subscriber.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Subscriber extends Model
+class Meeting extends Model
 {
 	use SoftDeletes;
 
