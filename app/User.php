2767ffd0e13a5ea903927c226adfc11b25365e0c
Anton <litto82@gmail.com>
2018-02-25 23:08:26 +0300

    Corrected

--- app/User.php
+++ app/User.php
@@ -51,6 +51,7 @@ class User extends Authenticatable implements JWTSubject
     {
         return $this->getKey();
     }
+    
     /**
      * Return a key value array, containing any custom claims to be added to the JWT.
      *
