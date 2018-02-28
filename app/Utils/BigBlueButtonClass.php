5daba71569937fc181e337f4e8723cffc6006301
Anton <litto82@gmail.com>
2018-02-28 14:53:03 +0300

    Formated code

--- app/Utils/BigBlueButtonClass.php
+++ app/Utils/BigBlueButtonClass.php
@@ -9,106 +9,134 @@ use BigBlueButton\Parameters\GetMeetingInfoParameters;
 use BigBlueButton\Parameters\GetRecordingsParameters;
 use BigBlueButton\Parameters\DeleteRecordingsParameters;
 use BigBlueButton\Parameters\IsMeetingRunningParameters;
-//
+
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\Exception\UnsatisfiedDependencyException;
 
-class BigBlueButtonClass 
+class BigBlueButtonClass
 {
-	
-	 public static function getMeetings(){
-			$bbb = new BigBlueButton();
-			$response = $bbb->getMeetings();
-			return $response;
+	 public static function getMeetings()
+	 {
+		$bbb = new BigBlueButton();
+		$response = $bbb->getMeetings();
 
-	 } 	
-	 public static function createMeeting($param=array()){
-			$meetingID = $param['meetingID'];
-			$meetingName=$param['meetingName'];
-			$attendee_password= $param['attendee_password'];
-			$moderator_password= $param['moderator_password'];
-			$duration = $param['duration'];
-			$urlLogout = $param['urlLogout'];
-			$isRecordingTrue= $param['isRecordingTrue'];		
-			$bbb = new BigBlueButton();			
-			
-			$createMeetingParams = new CreateMeetingParameters($meetingID, $meetingName);
-			$createMeetingParams->setAttendeePassword($attendee_password);
-			$createMeetingParams->setModeratorPassword($moderator_password);
-			$createMeetingParams->setDuration($duration);
-			$createMeetingParams->setLogoutUrl($urlLogout);
-			$createMeetingParams->setWelcomeMessage("Welcome Message Here!");
-			$createMeetingParams->setVoiceBridge("12345");
-			
-			if ($isRecordingTrue) {
+		return $response;
+	 }
 
-				$createMeetingParams->setRecord(true);
-				$createMeetingParams->setAllowStartStopRecording(true);
-				$createMeetingParams->setAutoStartRecording(true);
-			}
-			//print "<pre>";
-			//print_r($createMeetingParams);
-			//exit;
-			$response = $bbb->createMeeting($createMeetingParams);
-			return $response;
-			
+
+	 public static function createMeeting($param = [])
+	 {
+		$meetingID 	 = $param['meetingID'];
+		$meetingName = $param['meetingName'];
+		$attendee_password  = $param['attendee_password'];
+		$moderator_password = $param['moderator_password'];
+		$duration  = $param['duration'];
+		$urlLogout = $param['urlLogout'];
+		$isRecordingTrue = $param['isRecordingTrue'];
+		$bbb = new BigBlueButton();
+
+		$createMeetingParams = new CreateMeetingParameters($meetingID, $meetingName);
+		$createMeetingParams->setAttendeePassword($attendee_password);
+		$createMeetingParams->setModeratorPassword($moderator_password);
+		$createMeetingParams->setDuration($duration);
+		$createMeetingParams->setLogoutUrl($urlLogout);
+		$createMeetingParams->setWelcomeMessage("Welcome Message Here!");
+		$createMeetingParams->setVoiceBridge("12345");
+
+		if ($isRecordingTrue) {
+			$createMeetingParams->setRecord(true);
+			$createMeetingParams->setAllowStartStopRecording(true);
+			$createMeetingParams->setAutoStartRecording(true);
+		}
+		// print "<pre>";
+		// print_r($createMeetingParams);
+		// exit;
+		$response = $bbb->createMeeting($createMeetingParams);
+
+		return $response;
 	 }
-	 public static function joinMeeting($param=array()){
-			$meetingID 	= $param['meetingID'];
-			$name		= $param['name'];
-			$password	= $param['password'];
-			$bbb = new BigBlueButton();
-			$joinMeetingParams = new JoinMeetingParameters($meetingID, $name, $password); // $moderator_password for moderator
-			$joinMeetingParams->setRedirect(true);
-			$url = $bbb->getJoinMeetingURL($joinMeetingParams);
-			return $url;	
+
+
+	 public static function joinMeeting($param = [])
+	 {
+		$meetingID 	= $param['meetingID'];
+		$name		= $param['name'];
+		$password	= $param['password'];
+		$bbb = new BigBlueButton();
+		$joinMeetingParams = new JoinMeetingParameters($meetingID, $name, $password); // $moderator_password for moderator
+		$joinMeetingParams->setRedirect(true);
+		$url = $bbb->getJoinMeetingURL($joinMeetingParams);
+
+		return $url;
 	 }
-	 public static function closeMeeting($param=array()){
-			$meetingID 			= $param['meetingID'];
-			$moderator_password	= $param['moderator_password'] ;
-			$bbb = new BigBlueButton();
-			$endMeetingParams = new EndMeetingParameters($meetingID, $moderator_password);
-			$response = $bbb->endMeeting($endMeetingParams);	
-			return $response;
-		 
+
+
+	 public static function closeMeeting($param = [])
+	 {
+		$meetingID 			= $param['meetingID'];
+		$moderator_password	= $param['moderator_password'] ;
+		$bbb = new BigBlueButton();
+		$endMeetingParams = new EndMeetingParameters($meetingID, $moderator_password);
+		$response = $bbb->endMeeting($endMeetingParams);
+
+		return $response;
 	 }
-	 public static function getMeetingInfo($param=array()){
-			$meetingID 			= $param['meetingID'];
-			$moderator_password	= $param['moderator_password'] ;
-			$bbb = new BigBlueButton();
-			$getMeetingInfoParams = new GetMeetingInfoParameters($meetingID, '', $moderator_password);
-			$response = $bbb->getMeetingInfo($getMeetingInfoParams);
-			return $response;		 
+
+
+	 public static function getMeetingInfo($param = [])
+	 {
+		$meetingID 			= $param['meetingID'];
+		$moderator_password	= $param['moderator_password'] ;
+		$bbb = new BigBlueButton();
+		$getMeetingInfoParams = new GetMeetingInfoParameters($meetingID, '', $moderator_password);
+		$response = $bbb->getMeetingInfo($getMeetingInfoParams);
+
+		return $response;
 	 }
-	 public static function getRecordings($param=array()){
-			$meetingID 			= $param['meetingID'];
-			//$meetingID 			= 'df2ad546-a4f0-51ac-b38c-88216742e553';
-			$recordingParams = new GetRecordingsParameters();
-			//$recordingParams->setMeetingId($meetingID);
-			//$recordingParams ->setRecordId('fd51abbd107aa99ae7e38231c3ff57b31522911a-1503934326175');
-			$bbb = new BigBlueButton();
-			$response = $bbb->getRecordings($recordingParams);
-			return $response;
-	 }
-	 public static function deleteRecordings($param=array()){
-			$recordingID = $param['recordingID'];
-			$deleteRecordingsParams= new DeleteRecordingsParameters($recordingID); // get from "Get Recordings"
-			$bbb = new BigBlueButton();
-			$response = $bbb->deleteRecordings($deleteRecordingsParams);
-			return $response;
-			
-	 }
-	 public static function isMeetingRunning($param=array()){
-		 	$meetingID = $param['meetingID'];
-			$bbb = new BigBlueButton();
-			$isMeetingRunningParams= new IsMeetingRunningParameters($meetingID); 
-			$response = $bbb->isMeetingRunning($isMeetingRunningParams);
-			return $response;
-	 }
-	 
-	 public static function Uuid($string=''){
+
+
+	public static function getRecordings($param = [])
+	{
+		$meetingID 	  = $param['meetingID'];
+		// $meetingID = 'df2ad546-a4f0-51ac-b38c-88216742e553';
+		$recordingParams = new GetRecordingsParameters();
+		// $recordingParams->setMeetingId($meetingID);
+		// $recordingParams ->setRecordId('fd51abbd107aa99ae7e38231c3ff57b31522911a-1503934326175');
+		$bbb = new BigBlueButton();
+		$response = $bbb->getRecordings($recordingParams);
+
+		return $response;
+	}
+
+
+	public static function deleteRecordings($param = [])
+	{
+		$recordingID = $param['recordingID'];
+		// get from "Get Recordings"
+		$deleteRecordingsParams= new DeleteRecordingsParameters($recordingID);
+		$bbb = new BigBlueButton();
+		$response = $bbb->deleteRecordings($deleteRecordingsParams);
+
+		return $response;
+	}
+
+
+	public static function isMeetingRunning($param = [])
+	{
+	 	$meetingID = $param['meetingID'];
+		$bbb = new BigBlueButton();
+		$isMeetingRunningParams= new IsMeetingRunningParameters($meetingID);
+		$response = $bbb->isMeetingRunning($isMeetingRunningParams);
+
+		return $response;
+	}
+
+
+	public static function Uuid($string = '')
+	{
 		$uuid5 = Uuid::uuid5(Uuid::NAMESPACE_DNS, $string);
-		return $uuid5->toString();		
-	 }
-	 
+
+		return $uuid5->toString();
+	}
+
 }
