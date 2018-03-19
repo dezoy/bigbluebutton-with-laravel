1ff502f999235aec12b0a49c7bd9f8498672f211
Anton <litto82@gmail.com>
2018-03-19 10:21:00 +0300

    test

--- app/Http/Controllers/BigBlueButtonController.php
+++ app/Http/Controllers/BigBlueButtonController.php
@@ -41,7 +41,7 @@ class BigBlueButtonController extends Controller
 	{
 		$subscriber = Subscriber::where('hash', $userHash)->first();
 		$meeting 	= $subscriber->meeting();
-
+		print_r($meeting->toArray());
 		if (empty($meeting->createTime) ){
 			// Moderator has not joined yet
 			if ($subscriber->isModerator) {
